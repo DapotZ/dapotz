ba7d6d16b1bfbbf595c419bba828fcf15ee9349b
DapotZ <daffahakim378@gmail.com>
2024-10-30 03:59:55 +0700

    Updates

--- assets/ComingSoon-b8172177.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-565ae853.js";function i(){return o.jsx("div",{children:"coming soon page"})}export{i as default};
--- assets/Maintenance-e5de2f91.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-565ae853.js";function t(){return n.jsx("div",{children:"maintenance page"})}export{t as default};
